e4656e8267afc2bb31799a79191d928372e7e881
skmcloughlin <skmcloughlin@gmail.com>
2020-03-16 15:44:49 -0400

    11: used this binding of arrow func to set prop

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,14 +2,24 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef()
+  goToStore = (event) => {
+    event.preventDefault()
+    console.log(this.myInput)
+
+  }
   render() {
     return (
       <>
         {/* comment */}
-        <form action="" className="store-selector">
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name" defaultValue = {getFunName()} />
-          <button type="submit">Visit Store</button>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()} />
         </form>
       </>
     );}
