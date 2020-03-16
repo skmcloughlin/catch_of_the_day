0e1e93f841f12a07562273b71180be8c8cf7bbe5
skmcloughlin <skmcloughlin@gmail.com>
2020-03-16 13:51:27 -0400

    Finished lesson 9: Added Routing

---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+    render() {
+        return (
+            <h2 className='notFound'>
+                404: Not found
+            </h2>
+        );
+    }
+}
+
+export { NotFound };
