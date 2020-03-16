f2536c35f7a05b4ba9bea52b9b0932c442208513
skmcloughlin <skmcloughlin@gmail.com>
2020-03-16 12:03:37 -0400

    wote first global snippet

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,16 @@ import React from 'react'
 class App extends React.Component {
     render() {
         return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <header>
-
-                    </header>
-                    <Inventory>
-
-                    </Inventory>
-                    <Order></Order>
-                </div>
-        </div>
-    )
+          <div className="catch-of-the-day">
+            <div className="menu">
+              <header />
+            </div>
+            <div>
+              <Inventory />
+              <Order />
+            </div>
+          </div>
+        );
 }
 }
 
