bfa0faf943b968092761ccddd491e8268b375d19
skmcloughlin <skmcloughlin@gmail.com>
2020-03-16 16:47:23 -0400

    Finished 13: can set state from AddFishForm

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,30 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends React.Component {
-    render() {
-        return (
-          <div className="catch-of-the-day">
-            <div className="menu">
-              <Header tagline= "Fresh Seafood Market"/>
-            </div>
-            <div>
-              <Order />
-            </div>
-            <div>
-              <Inventory />
-            </div>
+  state = {
+    fishes: {},
+    order: {}
+  }
+  addFish = (fish) => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish
+    this.setState({ fishes })
+  }
+  render() {
+      return (
+        <div className="catch-of-the-day">
+          <div className="menu">
+            <Header tagline= "Fresh Seafood Market"/>
           </div>
-        );
-}
+          <div>
+            <Order />
+          </div>
+          <div>
+            <Inventory addFish={this.addFish} />
+          </div>
+        </div>
+      );
+  }
 }
 
 export { App };
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -7,7 +7,6 @@ class StorePicker extends React.Component {
     event.preventDefault()
     const storeName = this.myInput.current.value
     this.props.history.push(`/store/${storeName}`)
-
   }
   render() {
     return (
