99cc5f0e07136f35c804c354576da00b7c7abab1
skmcloughlin <skmcloughlin@gmail.com>
2020-03-16 11:11:06 -0400

    finished lesson 5

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <header>
+
+                    </header>
+                    <Inventory>
+
+                    </Inventory>
+                    <Order></Order>
+                </div>
+        </div>
+    )
+}
+}
+
+export { App };
