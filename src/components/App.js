304930edcae2ca3b7439911dd171fe536040056b
skmcloughlin <skmcloughlin@gmail.com>
2020-03-27 14:23:01 -0400

    Finished 16 - Updating Order State

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,24 @@ class App extends React.Component {
   loadSampleFishes = () => {
     this.setState({fishes: sampleFishes})
   }
+  addToOrder = (key) => {
+    const order = { ...this.state.order }
+    order[key] = order[key] + 1 || 1
+    this.setState({order})
+  }
+
   render() {
       return (
         <div className="catch-of-the-day">
           <div className="menu">
             <Header tagline="Fresh Seafood Market" />
             <ul className="fishes">
-              {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}></Fish> )}
+              {Object.keys(this.state.fishes).map(key =>
+                <Fish key={key}
+                  details={this.state.fishes[key]}
+                  addToOrder={this.addToOrder}
+                  index={key}
+                ></Fish>)}
             </ul>
           </div>
           <div>
